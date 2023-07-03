ecce4e4a3ae9f48593a97f7803a415070ddb9834
yeahn <yeahn@TONKA>
2023-07-03 03:57:53 -0400

    Add project files.

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Deliverable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer number between 1 and 100 ");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Specify the series type: Even or Odd");
+            string series = Console.ReadLine();
+
+            if (series == "Odd")
+            {
+                Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                for (int i = 1; i < input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            if (series == "Even")
+            {
+                Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are:");
+                for (int i = 0; i < input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
